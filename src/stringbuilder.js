515662f3bd091b1decf8f16920e7bb16b63f4673
Halil ŞAFAK <hibrahimsafak@gmail.com>
2020-11-10 12:10:42 +0300

    Invalid handling of numbers / falsy issue solved #13

--- src/stringbuilder.js
+++ src/stringbuilder.js
@@ -1,54 +1,62 @@
 "use strict";
 
-var Stream = require('stream').Stream;
+const Stream = require('stream').Stream;
 
-var StringBuilder = function StringBuilder(v) {
+const StringBuilder = function StringBuilder(v) {
     this.s = [];
     this.append(v);
     Stream.call(this);
 
     if (this.newline === undefined) {
-        var isWindows = process.platform == 'win32';
+        const isWindows = process.platform === 'win32';
         this.newline = isWindows ? '\r\n' : '\n';
     }
 };
 
 StringBuilder.prototype.append = function (v) {
-    if (v) {
+    if (typeof v === 'number' || typeof v === 'string') {
         this.s.push(v);
     }
+
     return this;
 };
 
 StringBuilder.prototype.appendLine = function (v) {
     this.s.push(this.newline);
-    if (v) {
+
+    if (typeof v === 'number' || typeof v === 'string') {
         this.s.push(v);
     }
+
     return this;
 };
 
 StringBuilder.prototype.appendFormat = function () {
-    var p = /({?){([^}]+)}(}?)/g;
-    var a = arguments, v = a[0], o = false;
-    if (a.length == 2) {
-        if (typeof a[1] == 'object' && a[1].constructor != String) {
+    const p = /({?){([^}]+)}(}?)/g;
+    let a = arguments, v = a[0], o = false;
+
+    if (a.length === 2) {
+        if (typeof a[1] == 'object' && a[1].constructor !== String) {
             a = a[1];
             o = true;
         }
     }
-    var s = v.split(p);
-    var r = [];
-    for (var i = 0; i < s.length; i += 4) {
+
+    const s = v.split(p);
+    const r = [];
+
+    for (let i = 0; i < s.length; i += 4) {
         r.push(s[i]);
+
         if (s.length > i + 3) {
-            if (s[i + 1] == '{' && s[i + 3] == '}') {
+            if (s[i + 1] === '{' && s[i + 3] === '}') {
                 r.push(s[i + 1], s[i + 2], s[i + 3]);
             } else {
                 r.push(s[i + 1], a[o ? s[i + 2] : parseInt(s[i + 2], 10) + 1], s[i + 3]);
             }
         }
     }
+
     this.s.push(r.join(''));
 };
 
--- test/main.js
+++ test/main.js
@@ -7,10 +7,26 @@ sb.appendLine();
 
 sb.appendFormat("formatted text {0},{1}", "format 1", "format 2");
 
+sb.appendLine()
+sb.append(12)
+sb.appendLine()
+sb.append(1)
+sb.appendLine()
+sb.append(0)
+sb.appendLine()
+sb.appendFormat("formatted text {0},{1}", 3.6745674, 12);
+sb.appendLine(true)
+sb.appendFormat("formatted text {0},{1}", 3.6745674, 1);
+
 console.log(sb.toString());
 
 /*
 console =>
 normal text
 formatted text format 1,format 2
+12
+1
+0
+formatted text 3.6745674,12
+formatted text 3.6745674,1
 */
