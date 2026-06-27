fe76ebfc78534bd4902df8baf78f833d64224a11
Halil ŞAFAK <hibrahimsafak@gmail.com>
2026-06-27 13:58:08 +0300

    refactor: convert to ES6 class syntax with proper Stream inheritance

--- src/stringbuilder.js
+++ src/stringbuilder.js
@@ -1,73 +1,71 @@
 "use strict";
 
-const Stream = require('stream').Stream;
+const { Stream } = require('stream');
 
-const StringBuilder = function StringBuilder(v) {
-    this.s = [];
-    this.append(v);
-    Stream.call(this);
-
-    if (this.newline === undefined) {
-        const isWindows = process.platform === 'win32';
-        this.newline = isWindows ? '\r\n' : '\n';
-    }
-};
-
-StringBuilder.prototype.append = function (v) {
-    if (v != null) {
-        this.s.push(v);
+class StringBuilder extends Stream {
+    constructor(v) {
+        super();
+        this.s = [];
+        this.newline = process.platform === 'win32' ? '\r\n' : '\n';
+        this.append(v);
     }
 
-    return this;
-};
-
-StringBuilder.prototype.appendLine = function (v) {
-    this.s.push(this.newline);
-
-    if (v != null) {
-        this.s.push(v);
-    }
-
-    return this;
-};
-
-StringBuilder.prototype.appendFormat = function () {
-    const p = /({?){([^}]+)}(}?)/g;
-    let a = arguments, v = a[0], o = false;
-
-    if (a.length === 2) {
-        if (typeof a[1] == 'object' && a[1].constructor !== String) {
-            a = a[1];
-            o = true;
+    append(v) {
+        if (v != null) {
+            this.s.push(v);
         }
+
+        return this;
     }
 
-    const s = v.split(p);
-    const r = [];
+    appendLine(v) {
+        this.s.push(this.newline);
 
-    for (let i = 0; i < s.length; i += 4) {
-        r.push(s[i]);
+        if (v != null) {
+            this.s.push(v);
+        }
 
-        if (s.length > i + 3) {
-            if (s[i + 1] === '{' && s[i + 3] === '}') {
-                r.push(s[i + 1], s[i + 2], s[i + 3]);
-            } else {
-                r.push(s[i + 1], a[o ? s[i + 2] : parseInt(s[i + 2], 10) + 1], s[i + 3]);
+        return this;
+    }
+
+    appendFormat() {
+        const p = /({?){([^}]+)}(}?)/g;
+        let a = arguments, v = a[0], o = false;
+
+        if (a.length === 2) {
+            if (typeof a[1] == 'object' && a[1].constructor !== String) {
+                a = a[1];
+                o = true;
             }
         }
+
+        const s = v.split(p);
+        const r = [];
+
+        for (let i = 0; i < s.length; i += 4) {
+            r.push(s[i]);
+
+            if (s.length > i + 3) {
+                if (s[i + 1] === '{' && s[i + 3] === '}') {
+                    r.push(s[i + 1], s[i + 2], s[i + 3]);
+                } else {
+                    r.push(s[i + 1], a[o ? s[i + 2] : parseInt(s[i + 2], 10) + 1], s[i + 3]);
+                }
+            }
+        }
+
+        this.s.push(r.join(''));
+
+        return this;
     }
 
-    this.s.push(r.join(''));
+    clear() {
+        this.s.length = 0;
+    }
 
-    return this;
-};
-
-StringBuilder.prototype.clear = function () {
-    this.s.length = 0;
-};
-
-StringBuilder.prototype.toString = function () {
-    return this.s.length === 0 ? '' : this.s.join('');
-};
+    toString() {
+        return this.s.length === 0 ? '' : this.s.join('');
+    }
+}
 
 module.exports = StringBuilder;
